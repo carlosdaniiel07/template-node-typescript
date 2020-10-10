21aaef706022c8c4d2f25b9336fce44faa453877
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2020-10-09 22:52:10 -0300

    Upload de arquivos com multer

--- src/routes/main/index.ts
+++ src/routes/main/index.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { Router } from 'express'
+import multer from 'multer'
 
 import cleanController from '../../middlewares/cleanController'
 import tokenValidator from '../../middlewares/tokenValidator'
@@ -11,9 +12,12 @@ import { Role } from '../../models/Auth'
 
 import validations from './validations'
 
+const upload = multer({ dest: './tmp/uploads/' })
+
 export default (router: Router) => {
   router.get('/', cleanController(MainController.index))
   router.get('/public-route', cleanController(MainController.index))
   router.get('/protected-route', tokenValidator([Role.MANAGER]), cleanController(MainController.index))
   router.post('/create', tokenValidator([Role.DEFAULT]), requestValidator(validations.createValidations), cleanController(MainController.index))
+  router.post('/file-upload', tokenValidator([Role.DEFAULT]), upload.single('file'), cleanController(MainController.index))
 }
